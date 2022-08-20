a0ef5aaaddf6905fbcbcd57458d9c11f09406447
Breno Fernando <brenofernandovieiradasilva.bd@gmail.com>
2022-08-20 16:04:30 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Brenofvs">
+    <meta name="application-name" content="Meu Pet Especial">
+    <meta name="description" content="Seu blog de auto ajuda para tutores de pets especiais. Porque todos os pets são especiais, mas alguns, mais!">
+    <meta name="keywords" content="Pet, Especiais, Doenças, Paralisia, Patologias, Cachorro, Gato">
+    <title>Meu Pet Especial</title>
+    <link rel="icon" href="./favicon.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+<header>
+
+</header>
+
+<body>
+
+</body>
+
+</html>
