4a490b564e44a52d679d627ea24a108fb07fc5ae
Breno Fernando <brenofernandovieiradasilva.bd@gmail.com>
2022-10-17 01:06:21 -0300

    responsive corrections

--- pages/home.php
+++ pages/home.php
@@ -5,8 +5,8 @@
             <div class="main-section">
                 <div class="main-banner">
                     <img id="main-banner" src="./imagens/pets/pet07.png" alt="">
-                    <div class="main-banner-data">
-                        <div id="main-banner-data" class="main-banner-container">
+                    <div id="main-banner-data" class="main-banner-data">
+                        <div class="main-banner-container">
                             <h3 class="font-menu cor-11">Nome do Pet</h3>
                             <p class="font-text-m">Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
                                 aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor
