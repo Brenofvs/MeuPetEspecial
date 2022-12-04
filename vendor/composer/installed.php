b9347b8f47a7189dc42ed3cddba2c5b5ed93f244
Breno Fernando <brenofernandovieiradasilva.bd@gmail.com>
2022-12-04 10:52:53 -0300

    backend

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'brenofvs/fsphp',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dca5ccef88c302fbc4d34c60b4a9b10a3c4d54e3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'brenofvs/fsphp' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dca5ccef88c302fbc4d34c60b4a9b10a3c4d54e3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.6.5',
+            'version' => '6.6.5.0',
+            'reference' => '8b6386d7417526d1ea4da9edb70b8352f7543627',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
