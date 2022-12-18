3076f8d515e3b0120b9b4adc9a31e5ca136b6da8
Breno Fernando <brenofernandovieiradasilva.bd@gmail.com>
2022-12-18 20:28:01 -0300

    script correction

--- scripts/mainBannerData.php
+++ scripts/mainBannerData.php
@@ -13,5 +13,5 @@ $result = $post->find("image = :img", "img={$imgSrc}");
 echo "<div class='main-banner-container'>
     <h3 class='font-menu cor-11'>" . mb_strimwidth("{$result->title}", 0, 40, "...") . "</h3>
     <p class='font-text-m'>" . mb_strimwidth("{$result->body}", 0, 150, "...") . "</p>
-    <a href='./materia?id={$result->id}' class='botao'>Saiba Mais</a>
+    <a href='./materia&id={$result->id}' class='botao'>Saiba Mais</a>
 </div>";
