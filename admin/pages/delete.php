792de17feb52b48200bf0f1c4bcbd055d966240f
Breno Fernando <brenofernandovieiradasilva.bd@gmail.com>
2022-12-19 14:21:04 -0300

    Delete corrections

--- admin/pages/delete.php
+++ admin/pages/delete.php
@@ -27,12 +27,32 @@ if (!is_null($postDel)) {
     } else {
         include('./forms/form-delete.php');
     }
-} else {
-    if ($_GET['delete'] === "true") {
-        $message->success("Matéria deletada com sucesso!");
-    } else {
-        $message->error("Parece que a matéria que está tentando excluir não existe mais!");
-    }
+} elseif (is_null($postDel) && !isset($_GET['delete'])) {
+    $message->error("Parece que essa matéria já foi deletada!");
+    echo "
+    <body>
+    <main class='d-flex w-100'>
+        <div class='container-fluid d-flex flex-column'>
+            <div class='row'>
+                <div class='col-sm-12 col-md-11 col-lg-10 mx-auto d-table h-100'>
+                    <div class='d-table-cell align-top'>
+                        <div class='text-center mt-4'>
+                                <div class='d-flex mb-4 justify-content-between'>
+                                    <h1 class='h2'>Excluir matéria!</h1>
+                                    <a href='?page=materias' class='btn btn-primary d-inline-flex justify-content-center align-items-center fs-5'><i class='align-middle me-2' data-feather='arrow-left-circle'></i>Voltar</a>
+                                </div>
+                                {$message->render()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    </body>";
+}
+if (isset($_GET['delete']) && $_GET['delete'] === "true") {
+    $message->success("Matéria deletada com sucesso!");
     echo "
     <body>
     <main class='d-flex w-100'>
